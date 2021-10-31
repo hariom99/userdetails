337b699d47272bd96864b26c6b6eb7b53be852a3
hariom kourav <hariomkourav1999@gmail.com>
2021-10-31 14:12:16 +0530

    app updated dom to react

--- src/component/userDetails.js
+++ src/component/userDetails.js
@@ -3,20 +3,28 @@ import React, { Component } from "react";
 export class UserDetails extends Component {
 
 
+
     render() {
-        console.log(this.props.details[0].length);
-        let temp = this.props.details;
-        let a = []
-        for (let j = 1; j < temp.length; j++) {
-
-            // for (let i = 0; i < temp[j].length; i++)
-            a.push(<div className="user" key={j}> Name : {temp[j][0]} | Department:{temp[j][1]} | Rating: {temp[j][2]} </div>)
-        }
-
+        console.log(this.props.details);
         return (
+            <div className="details" >
 
-            <div className="details" >{a}</div>
+
+                {this.props.details.map((val, key) => {
+                    return <div className="user" key={key} >
+                        Name : {val.name} |
+                        Department : {val.dep} |
+                        Rating : {val.rating}
+                    </div>
+
+                })}
+
+
+            </div>
         );
 
     }
 }
+
+
+{/* <div className="user" */ }
--- src/component/userForm.js
+++ src/component/userForm.js
@@ -1,33 +1,23 @@
 import React, { Component } from "react";
-import { UserDetails } from "./userDetails";
 export class UserForm extends Component {
     state = { name: null, dep: null, rating: null };
     data = [[1, 2, 3]];
 
+    setName = (event) => {
 
-    getDetails = () => {
-        // this.data.push("ip");
-        let ip = [];
-        // ip[0] = "";
-        // ip[1] = "";
-        // ip[2] = "";
-        // this.data.push(ip);
-        ip[0] = document.getElementById("name").value;
-        ip[1] = document.getElementById("dep").value;
-        ip[2] = document.getElementById("rating").value;
-        document.getElementById("name").value = "";
-        document.getElementById("dep").value = "";
-        document.getElementById("rating").value = "";
-        this.data.push(ip);
-        // let name = document.getElementById("name").value;
-        // let dep = document.getElementById("dep").value;
-        // let rating = document.getElementById("rating").value;
-        // console.log(ip[0] + " " + ip[1] + " " + ip[2]);
-        // console.log(this.data);
-        this.setState({ name: "hk" });
-        return ip;
+        this.setState({ name: event.target.value });
+    }
+    setDep = (event) => {
+        this.setState({ dep: event.target.value });
+    }
+    setRat = (event) => {
+        this.setState({ rating: event.target.value });
     }
 
+    check = () => {
+        this.props.addDetails(this.state.name, this.state.dep, this.state.rating);
+
+    }
 
     render() {
 
@@ -35,16 +25,56 @@ export class UserForm extends Component {
         return <div>
             <h1 style={{ textAlign: "center" }} >EMPLOYEE FEEDBACK FORM</h1>
             <div className="form-div" >
-                <span>Name</span> <input id="name" className="inp" placeholder="   enter name" /><br />
-                <span>Department</span><input id="dep" className="inp" placeholder="  enter department" /><br />
-                <span>Rating</span><input id="rating" className="inp" placeholder="  enter rating" />
+                <span>Name</span> <input id="name" className="inp" onChange={this.setName} placeholder="   enter name" /><br />
+                <span>Department</span><input id="dep" className="inp" onChange={this.setDep} placeholder="  enter department" /><br />
+                <span>Rating</span><input id="rating" className="inp" onChange={this.setRat} placeholder="  enter rating" />
                 <br />
-                <input className="submit-btn" onClick={this.getDetails} type="submit" />
+                <input className="submit-btn" onClick={this.check} type="submit" />
             </div>
-            <div className="ipDetails" >
-                <hr className="hr" />
-                <UserDetails details={this.data} />
-            </div>
+            <hr className="hr" />
+
         </div>
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// getDetails = () => {
+//     // this.data.push("ip");
+//     let ip = [];
+//     // ip[0] = "";
+//     // ip[1] = "";
+//     // ip[2] = "";
+//     // this.data.push(ip);
+//     ip[0] = document.getElementById("name").value;
+//     ip[1] = document.getElementById("dep").value;
+//     ip[2] = document.getElementById("rating").value;
+//     document.getElementById("name").value = "";
+//     document.getElementById("dep").value = "";
+//     document.getElementById("rating").value = "";
+//     this.data.push(ip);
+//     // let name = document.getElementById("name").value;
+//     // let dep = document.getElementById("dep").value;
+//     // let rating = document.getElementById("rating").value;
+//     // console.log(ip[0] + " " + ip[1] + " " + ip[2]);
+//     // console.log(this.data);
+//     this.setState({ name: "hk" });
+//     return ip;
+// }
